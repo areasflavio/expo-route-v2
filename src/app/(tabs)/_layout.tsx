42f3ec68170d8079e0055ffc9ef0891e67754f87
Flávio Arêas <vflavio9@gmail.com>
2023-07-16 11:19:59 -0300

    feat: set up navigation

--- src/app/(tabs)/_layout.tsx
+++ src/app/(tabs)/_layout.tsx
@@ -1,10 +1,44 @@
+import { MaterialIcons } from '@expo/vector-icons';
 import { Tabs } from 'expo-router';
 
 export default function TabsRoutesLayout() {
   return (
-    <Tabs>
-      <Tabs.Screen name="index" options={{ title: 'Home' }} />
-      <Tabs.Screen name="profile" options={{ title: 'Profile' }} />
+    <Tabs screenOptions={{ headerShown: false }}>
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: 'Home',
+          tabBarIcon: (props) => (
+            <MaterialIcons name="home" size={props.size} color={props.color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="profile"
+        options={{
+          title: 'Profile',
+          tabBarIcon: (props) => (
+            <MaterialIcons
+              name="person"
+              size={props.size}
+              color={props.color}
+            />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="settings"
+        options={{
+          title: 'Settings',
+          tabBarIcon: (props) => (
+            <MaterialIcons
+              name="settings"
+              size={props.size}
+              color={props.color}
+            />
+          ),
+        }}
+      />
     </Tabs>
   );
 }
